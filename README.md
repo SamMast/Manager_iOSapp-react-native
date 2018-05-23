987e092791db2d417c77311ff32fb8e9aec04eaa
Samuel Mast <samuel.a.mast@gmail.com>
2018-05-22 21:18:32 -0600

    added delete and text functionality for manager

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# Manager_iOSapp-react-native
+# React-Native Manager App
+
+- Mobile App to Manage Employee's and Shifts
+
+
+# Purpose 
+
+* Users can sign in to access their manager portal.
+
+* Once logged in, Users can add employee names, phone numbers, and schedule day of the week. 
+
+* Managers can then Edit the list of Employees, Delete them, and even text them their next shift. 
+
+
+## App Functionality:
+
+- App uses React-native with Redux 
+
+- React-native Scenes used for page changes
+
+- Texting using React-Native-Communications
+
+- Data persists through firebase and is stored by user
+
+
+### Screenshots:
+
+![Image 1](./images/image1.png)
+
+
+![Image 2](./images/image2.png)
+
+
+![Image 3](./images/image3.png)
+
+
+![Image 4](./images/image3.png)
+
+
+![Image 5](./images/image3.png)
+
+
+![Image 6](./images/image3.png)
--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,11 +1,14 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { employeeUpdate, employeeSave } from '../actions';
-import { Card, CardSection, Button } from '../components/common/';
+import Communications from 'react-native-communications';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
+import { Card, CardSection, Button, Confirm } from './common';
 import EmployeeForm from './EmployeeForm';
 
 class EmployeeEdit extends Component {
+	state = { showModal: false };
+
 	componentWillMount() {
 		_.each(this.props.employee, (value, prop) => {
 			this.props.employeeUpdate({ prop, value });
@@ -18,15 +21,50 @@ class EmployeeEdit extends Component {
 		this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
 	}
 
+	onTextPress() {
+		const { name, phone, shift } = this.props;
+
+		Communications.text(phone, `Hey ${name}, Your upcoming Shift is on ${shift}`);
+	}
+
+	onAccept() {
+		this.props.employeeDelete({ uid: this.props.employee.uid });
+	}
+
+	onDecline() {
+		this.setState({ showModal: false });
+	}
+
 	render() {
 		return (
 			<Card>
 				<EmployeeForm {...this.props} />
+
 				<CardSection>
 					<Button onPress={this.onButtonPress.bind(this)}>
 						Save Changes
 					</Button>
 				</CardSection>
+
+				<CardSection>
+					<Button onPress={this.onTextPress.bind(this)}>
+						Text Employee Schedule
+					</Button>
+				</CardSection>
+
+				<CardSection>
+					<Button onPress={() => this.setState({ showModal: !this.state.showModal })}>
+						Delete Employee
+					</Button>
+				</CardSection>
+
+				<Confirm
+					visible={this.state.showModal}
+					onAccept={this.onAccept.bind(this)}
+					onDecline={this.onDecline.bind(this)}
+				>
+					Are you sure you want to delete '{this.props.name}' from the system?
+				</Confirm>
 			</Card>
 		);
 	}
@@ -38,4 +76,4 @@ const mapStateToProps = (state) => {
 	return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave, employeeDelete })(EmployeeEdit);
